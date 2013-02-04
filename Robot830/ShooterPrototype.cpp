b9f1e59c907ee1732a80accd57a211e8fb890d1a
ratpack <scottemorton@comcast.net>
2013-02-04 18:39:42 -0500

    fixed everything

--- Robot830/ShooterPrototype.cpp
+++ Robot830/ShooterPrototype.cpp
@@ -1,17 +1,15 @@
 #include "WPILib.h"
-#include <iostream>
-using namespace::std;
-// I changed this
 
 class ShooterPrototype : public IterativeRobot {
 	Joystick joystick;
 	Victor flywheel;
+	DriverStationLCD* ds;
 	
 	static const int JOYSTICK_PORT = 1;
 	static const int FLYWHEEL_PWM = 1;
 public:
-	ShooterPrototype() : joystick(JOYSTICK_PORT), flywheel(FLYWHEEL_PWM){
-		
+	ShooterPrototype() : joystick(JOYSTICK_PORT), flywheel(FLYWHEEL_PWM) {
+		ds = DriverStationLCD::GetInstance();
 	}
 	
 	void RobotInit(){
@@ -40,12 +38,17 @@ public:
 	
 	void TeleopPeriodic(){
 		float throttle = joystick.GetThrottle();
+		//map [1, -1] throttle values to [0, 1] victor speeds
 		throttle = (-throttle + 1)/2; 
-		cout << "Throttle is at " << throttle << "%" << endl;
+		//clamp to multiples of 0.05
+		throttle = 0.05 * (int)(throttle * 20);
+		ds->PrintfLine(DriverStationLCD::kUser_Line2, "Throttle at: %d%%", (int) throttle * 100);
 		if (joystick.GetTrigger()){
 			flywheel.Set(throttle);
+			ds->PrintfLine(DriverStationLCD::kUser_Line1, "TRIGGER ENGAGED");
 		} else {
 			flywheel.Set(0);
+			ds->PrintfLine(DriverStationLCD::kUser_Line1, "TRIGGER DISENGAGED");
 		}
 	}
 	
